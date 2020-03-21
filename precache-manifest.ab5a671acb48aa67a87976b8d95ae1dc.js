b9ff6de957f8510dd965b04ad0ee37dc53c7633c
Torrence Cole <trcole@gmail.com>
2020-03-21 12:04:50 -0400

    Updates

---
+++ precache-manifest.ab5a671acb48aa67a87976b8d95ae1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc731e7dbd551f7528c753583f51585",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "7b1e2936b369a602f2e3",
+    "url": "/js-calculator/static/css/main.703cd0eb.chunk.css"
+  },
+  {
+    "revision": "6b40dd9f1086c0b4a7d8",
+    "url": "/js-calculator/static/js/2.015aa9e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/js-calculator/static/js/2.015aa9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1e2936b369a602f2e3",
+    "url": "/js-calculator/static/js/main.f1e08806.chunk.js"
+  },
+  {
+    "revision": "f0ca4db7085c59c0e9ef",
+    "url": "/js-calculator/static/js/runtime-main.7a08d950.js"
+  }
+]);
